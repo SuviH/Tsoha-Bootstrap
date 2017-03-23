f00ebeb21e94caf1a3b2d41a5fdedd8c6c524cbc
SuviH <suvi.i.heinonen@helsinki.fi>
2017-03-23 23:56:19 +0200

    tietokantalauseita

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,34 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY, 
+  nimi varchar(50) NOT NULL, 
+  salasana varchar(50) NOT NULL
+);
+CREATE TABLE Drinkki(
+  id SERIAL PRIMARY KEY,
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  nimi varchar(50) NOT NULL,
+  kuvaus varchar(400),
+  lisayspaiva DATE,
+  tyyppi INTEGER REFERENCES Tyyppi(id),
+  valmistusohje varchar(1000)
+);
+CREATE TABLE RaakaAine(
+  id SERIAL PRIMARY KEY,
+  nimi varchar(50) NOT NULL
+);
+CREATE TABLE Mittayksikko(
+  id SERIAL PRIMARY KEY,
+  nimi varchar(50) NOT NULL
+);
+CREATE TABLE Ainesosa(
+  id SERIAL PRIMARY KEY,
+  drinkki_id INTEGER REFERENCES Drinkki(id),
+  raakaAine_id INTEGER REFERENCES RaakaAine(id),
+  mitta_id INTEGER REFERENCES Mittayksikko(id),
+  maara integer
+);
+CREATE TABLE Tyyppi(
+  id SERIAL PRIMARY KEY,
+  nimi varchar(50)
+);
